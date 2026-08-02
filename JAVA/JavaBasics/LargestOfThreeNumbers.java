4ef9dbaf406566edeacfdff6f95c06d588d69935
Annvalentina13 <annievalentina1307@gmail.com>
2026-08-02 20:44:27 +0530

    Largest Of Three Numbers

--- JAVA/JavaBasics/LargestOfThreeNumbers.java
+++ JAVA/JavaBasics/LargestOfThreeNumbers.java
@@ -24,16 +24,14 @@ public class LargestOfThreeNumbers {
         System.out.println("Enter number 3: ");
         int num3 = scanner.nextInt();
 
-        if(num1 > num2 && num1 > num3){
-            System.out.println(num1);
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("Largest Number = " + num1);
         }
-
-        else if(num2 > num1 && num2 > num3){
-            System.out.println(num2);
+        else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("Largest Number = " + num2);
         }
-
-        else{
-            System.out.println(num3);
+        else {
+            System.out.println("Largest Number = " + num3);
         }
 
     }
