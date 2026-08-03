ed7193bf520a987aa17835253535d002c1a872c8
Annvalentina13 <annievalentina1307@gmail.com>
2026-08-03 19:13:35 +0530

    SQL Basics: Database Creation (BookStore DB)

---
+++ SQL/SQL Basics/DatabaseCreation(BookStoreDB).sql
@@ -0,0 +1,7 @@
+-- Create a database named BookStore and make it the current working database.
+-- Hint
+-- Use the command to create a new database.
+-- After creating it, switch to that database before creating any tables.
+
+CREATE DATABASE BookStore;
+USE BookStore;
